0afc362739e68e1975c34d245d8034663b9e0046
Steven Valentin <stevenvalen10@gmail.com>
2024-05-25 15:49:25 -0400

    Update

---
+++ javaTest.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+class Users{
+    String name;
+    String email;
+
+    Users(String name, String email){
+        this.name = name;
+        this.email = name + '@javatest.com';
+    }
+}
+
+
+
+class JavaTest{
+    public static void main(String[] arg) {
+        System.out.println('Start program');
+        Users u1 = new Users(Luis)
+        System.out.println('End program');
+    }
+
+    public JavaTest(String users){
+
+    }
+}
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+
+List<String> email=new ArrayList<String>();  
+        Collections.sort(email);
+
+3. The console should always print at the end of the execution: "End program."
+
+4. You must store in memory based on the list of names provided as follows:
+
+4-A. The name must be stored in proper name format.
+
+4-B. The email must be stored based on the name provided by adding the domain "@ javatest.com".
+
+ String s1 = name.substring(0, 1).toUpperCase();
+
+Name list: Luis, maria, laura, Marta, Juan.
